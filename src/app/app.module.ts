a8a30aed9cc3633824357c2191b95163bd9b7f74
Dinesh bade <dineshbade1992@gmail.com>
2017-10-27 15:02:31 +0545

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    LoginComponent,
+    RegistrationComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
